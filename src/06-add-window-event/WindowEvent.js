e92753864c48456091cf80f66bc0164a82f7e2ff
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2023-01-03 11:47:23 -0600

    my solution challenge 6

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,15 @@
-export default function WindowEvent () {
-  return (
-    <h2>Window event active</h2>
-  )
+import { useEffect } from 'react';
+
+export default function WindowEvent() {
+  useEffect(() => {
+    function myAlert(e) {
+      window.alert('You just doubleClicked');
+    }
+
+    window.addEventListener('dblclick', myAlert);
+
+    return () => window.removeEventListener('dblclick', myAlert);
+  }, []);
+
+  return <h2>Window event active</h2>;
 }
